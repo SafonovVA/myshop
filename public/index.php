723a354e9a4482a591b24669a8df62eebcf1f89b
Safonov <safonov.open@gmail.com>
2019-09-12 04:32:03 +0600

    rotes part 1

--- public/index.php
+++ public/index.php
@@ -2,9 +2,7 @@
 
 require_once dirname(__DIR__).'/config/init.php';
 require_once LIBS . '/functions.php';
+require_once CONF . '/routes.php';
 
 new \myshop\App();
-//\myshop\App::$app->setProperty('test', 'TEST');
-//debug(\myshop\App::$app->getProperties());
-
-throw new Exception('Страница не найдена', 404);
+new Exception('Страница не найдена', 404);
---
+++ vendor/myshop/core/Router.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace myshop;
+
+
+class Router
+{
+
+    protected static $routes = [];
+    protected static $route = [];
+
+    public static function add($regexp, $route = []) {
+        self::$routes[$regexp] = $route;
+    }
+
+    public static function getRoutes() {
+        return self::$routes;
+    }
+
+    public static function getRoute() {
+        return self::$route;
+    }
+
+    public static function dispatch($url) {
+
+    }
+
+    public static function matchRoute($url) {
+
+    }
+}
